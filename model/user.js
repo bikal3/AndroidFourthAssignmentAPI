74cfc1ade32e66e840ac379555890ec15f5aea34
bikal3 <email_id>
2019-05-31 16:17:10 +0545

    android API

---
+++ model/user.js
@@ -0,0 +1,23 @@
+const moongose = require('mongoose');
+
+const User = moongose.model('User', {
+
+    userId: {
+        type: String
+    },
+    userFname: {
+        type: String
+    },
+    userLname: {
+        type: String
+    },
+    username: {
+        type: String
+    },
+    password: {
+        type: String
+    }
+
+
+})
+module.exports = User;
